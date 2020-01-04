eac2d1deac07da83c72a56596aac71d7b63e02e1
jasshan29goel <goeljashan29@gmail.com>
2020-01-05 03:04:33 +0530

    deploy

--- server.js
+++ server.js
@@ -4,6 +4,7 @@ const connectDB = require('./config/db');
 const users = require('./routes/api/users');
 const auth = require('./routes/api/auth');
 const question = require('./routes/api/question');
+const path = require('path');
 
 const app = express();
 
@@ -20,8 +21,16 @@ app.use('/api/users', users);
 app.use('/api/auth', auth);
 app.use('/api/question', question);
 
-
-app.get('/',(req,res) => res.send('API Running') )
+// Serve static assets in production
+if (process.env.NODE_ENV === 'production') {
+    // Set static folder
+    app.use(express.static('client/build'));
+  
+    app.get('*', (req, res) => {
+      res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
+    });
+  }
+  
  
 const port = process.env.PORT || 5000;
 
